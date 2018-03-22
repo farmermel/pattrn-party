d3373d64acfc14390a6544bc71094aa86530af62
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-03-22 16:56:13 -0600

    Account for excess padding being clicked and not corresponding to a tab id, defaults to currently selected when undefined

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,18 @@
 
 const toggleTabs = (e) => {
   $('#drop-tab').remove();
-  const tabId = $(e.target).attr('id');
-  $('div .active').removeClass('active');
-  $(`#${tabId}`).addClass('active');
-  $(`#${tabId}`).append(`
+  const tabClass = $(e.target).attr('class') || $('div #active').attr('class');
+  $('div #active').removeAttr('id', 'active');
+  $(`.${tabClass}`).attr('id', 'active');
+  $(`.${tabClass}`).append(`
     <article id='drop-tab'>
-      
-      <h2>Annoy the ${tabId} grumpy cat, start</h2>
-      <hr />
-      <p>Destroy house in 5 seconds cat chases laser jumps in bathtub and meows when owner fills food dish the cat knocks over the food dish cat slides down the water slide and into pool and swims even though it does not like water or paw at beetle,
-      <br />
-      <br />
-
-       meow loudly at 5am next to human slave food dispenser. howl uncontrollably for no reason hide head hate dog, for give me some of your food give me some of your food give me some of your food meh, i don't want it</p>
+      <img class='tab-plcholder-img' src='assets/placeholder.png' alt='placeholder image' />
+      <div>
+        <h2>Annoy the ${tabClass} grumpy cat, start</h2>
+        <hr />
+        <p>Destroy house in 5 seconds cat chases laser jumps in bathtub and meows when owner fills food dish the cat knocks over the food dish cat slides down the water slide and into pool and swims even though it does not like water or paw at beetle</p>
+        <p>meow loudly at 5am next to human slave food dispenser. howl uncontrollably for no reason hide head hate dog, for give me some of your food give me some of your food give me some of your food meh, i don't want it</p>
+      </div>
     </article>
   `)
 }
