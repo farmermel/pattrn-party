f82a1cc6a940a879c3b9831312f9bfb874042e64
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-03-22 06:59:32 -0600

    Add active class to current tab and html for current tab contents

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,20 @@
 
 const toggleTabs = (e) => {
-  //clear tabs of appended things
+  $('#drop-tab').remove();
   const tabId = $(e.target).attr('id');
+  $('div .active').removeClass('active');
+  $(`#${tabId}`).addClass('active');
   $(`#${tabId}`).append(`
-    <article> Lorem ipsum etc</article>
+    <article id='drop-tab'>
+      
+      <h2>Annoy the ${tabId} grumpy cat, start</h2>
+      <hr />
+      <p>Destroy house in 5 seconds cat chases laser jumps in bathtub and meows when owner fills food dish the cat knocks over the food dish cat slides down the water slide and into pool and swims even though it does not like water or paw at beetle,
+      <br />
+      <br />
+
+       meow loudly at 5am next to human slave food dispenser. howl uncontrollably for no reason hide head hate dog, for give me some of your food give me some of your food give me some of your food meh, i don't want it</p>
+    </article>
   `)
 }
 
