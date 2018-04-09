8d921e6d368be0a16c360f319374abd2d436b69d
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-04-08 19:13:44 -0600

    Toggle function for menu dropdown

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,5 @@
 
-const toggleTabs = (e) => {
+const toggleTabs = e => {
   $('#drop-tab').remove();
   const tabClass = $(e.target).attr('class') || $('div #active').attr('class');
   $('div #active').removeAttr('id', 'active');
@@ -17,4 +17,10 @@ const toggleTabs = (e) => {
   `)
 }
 
-$('#tab-wrap').on('click', toggleTabs)
+$('#tab-wrap').on('click', toggleTabs);
+
+const toggleMenu = e => {
+  $('.header-dropdown-wrap').toggleClass('show-menu');
+}
+
+$('.dropdown-menu-label').on('click', toggleMenu);
